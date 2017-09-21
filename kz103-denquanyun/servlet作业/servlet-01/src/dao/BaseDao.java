3c3924de1ab80852392064033684104f9f4c5b10
kate-chan <1545991585@qq.com>
2017-09-21 21:08:41 +0800

    servlet第一次第二次作业

---
+++ kz103-denquanyun/servlet作业/servlet-01/src/dao/BaseDao.java
@@ -0,0 +1,47 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BaseDao {
+	public static Connection getConn(){
+		Connection conn = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","scott","abc");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	public static void closeConn(ResultSet rs,PreparedStatement pst,Connection conn){
+		try {
+			if(rs!=null){
+				rs.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			if(pst!=null){
+				pst.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			if(conn!=null){
+				conn.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
