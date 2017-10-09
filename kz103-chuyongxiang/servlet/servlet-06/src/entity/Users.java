dbde42b0dc1846a789f569171d3af3c2e072ae10
kate-chan <771056832@qq.com>
2017-10-09 23:30:15 +0800

    第六次

---
+++ kz103-chuyongxiang/servlet/servlet-06/src/entity/Users.java
@@ -0,0 +1,43 @@
+package entity;
+
+public class Users {
+
+	private int id;
+	
+	private String uname;
+	
+	private String upwd;
+
+	
+	
+	@Override
+	public String toString() {
+		return "Users [id=" + id + ", uname=" + uname + ", upwd=" + upwd + "]";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUname() {
+		return uname;
+	}
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+	public String getUpwd() {
+		return upwd;
+	}
+
+	public void setUpwd(String upwd) {
+		this.upwd = upwd;
+	}
+	
+	
+}
