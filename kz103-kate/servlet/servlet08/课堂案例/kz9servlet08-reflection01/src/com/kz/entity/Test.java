f64bd2c8e42f4c531360672cc884b3a0c0e6ccb9
kate-tanzhou <2315611897@qq.com>
2017-10-10 11:28:12 +0800

    serlvet第8次

---
+++ kz103-kate/servlet/servlet08/课堂案例/kz9servlet08-reflection01/src/com/kz/entity/Test.java
@@ -0,0 +1,10 @@
+package com.kz.entity;
+
+public class Test {
+	public static void main(String[] args) {
+		//使用Student.class文件来创建对象
+		Student s = new Student();
+		s.sayHello();
+		Student s1 = new Student();
+	}
+}
