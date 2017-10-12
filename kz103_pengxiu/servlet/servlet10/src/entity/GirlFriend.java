99f43a7e19e055f33906dd54093494ae7fbcac3a
pwngxiu <2830882828@qq.com>
2017-10-12 23:09:54 +0800

    彭修第10次作业

---
+++ kz103_pengxiu/servlet/servlet10/src/entity/GirlFriend.java
@@ -0,0 +1,21 @@
+package entity;
+
+public class GirlFriend implements Cloneable{
+	public String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return super.clone();
+	}
+	
+	
+}
---
+++ kz103_pengxiu/servlet/servlet10/src/test/test_01.java
@@ -0,0 +1,29 @@
+package test;
+
+import java.lang.reflect.Field;
+
+import dao.BaseDao;
+import entity.A;
+import entity.emp;
+
+public class test_01 {
+	public static void main(String[] args) throws Exception {
+//		A a=new A();
+//		a.setAid(5);
+//		a.setName("ee");
+//		int save = BaseDao.save(a,"aid");
+//		System.out.println(save);
+//		emp e=new emp();
+//		e.setCOMM(1);
+//		e.setDEPTNO(2);
+//		e.setEMPNO(3);
+//		e.setENAME("s");
+//		e.setHIREDATE("10-12ÔÂ-17");
+//		e.setJOB("jOB");
+//		e.setMGR(12);
+//		e.setSAL(12);
+//		System.out.println(BaseDao.save(e, "EMPNO"));
+		
+		
+	}
+}
