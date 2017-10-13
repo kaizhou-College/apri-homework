ea28eb40b0ffb541ad3de7c2555c7b89682c406b
pwngxiu <2830882828@qq.com>
2017-10-13 15:56:52 +0800

    彭修最新第十次作业提交

--- kz103_pengxiu/servlet/servlet10/src/test/text_02.java
+++ kz103_pengxiu/servlet/servlet10/src/test/text_02.java
@@ -5,30 +5,30 @@ import entity.stl;
 
 public class text_02 {
 //4.写一个简单的例子，实现浅度克隆和深度克隆的例子
-	public static void main(String[] args) {
-		stl s = new stl();
-		s.setName("a");
-		s.setSec("aa");
-		
-		stl s2 = s;
-		s2.setName("bb");
-		System.out.println(s.getName());
-		System.out.println(s2.getName());
+	public static void main(String[] args) throws Exception {
+//		stl s = new stl();
+//		s.setName("a");
+//		s.setSec("aa");
+//		
+//		stl s2 = s;
+//		s2.setName("bb");
+//		System.out.println(s.getName());
+//		System.out.println(s2.getName());
 		
 		
 	//深度
-		stl ss = new stl();
-		s.setName("daf");
+		/*stl ss = new stl();
+		ss.setName("daf");
 		GirlFriend gfs = new GirlFriend();
 		gfs.setName("潘大妈");
-		s.setGf(gfs);
+		ss.setGf(gfs);
 		
-		stl ss2 = (stl) s.clone();
-		s2.setUname("苏大妈");
+		stl ss2 = (stl) ss.clone();
+		ss2.setName("苏大妈");
 		//改邓大妈的女朋友的名字
-		s2.getGf().setName("康大妈222");
+		ss2.getGf().setName("康大妈222");
 		System.out.println(ss.getGf().getName());
-		System.out.println(ss2.getGf().getName());
+		System.out.println(ss2.getGf().getName());*/
 	}
 	
 }
