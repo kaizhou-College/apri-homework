8937ba8be640d8dbe5af2925324166b73401b71d
kate-chan <771056832@qq.com>
2017-10-24 21:58:03 +0800

    Struts2第一次

---
+++ kz103-chuhuandong/servlet/第三作业/kz02servlet/src/com/kz/servlet/servlet01.java
@@ -0,0 +1,30 @@
+package com.kz.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class servlet01 extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		request.setCharacterEncoding("utf-8");
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("name");
+		String pwd = request.getParameter("pwd");
+		out.print("Œ“ «get"+name+"----"+pwd);
+	}
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		request.setCharacterEncoding("utf-8");
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("name");
+		String pwd = request.getParameter("pwd");
+		out.print("Œ“post"+name+"----"+pwd);
+	}
+}
